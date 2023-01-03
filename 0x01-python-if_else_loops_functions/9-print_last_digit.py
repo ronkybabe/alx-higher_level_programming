88e695e96b71c66ebcbf11a965e650279045bb8d
Ronke Ashaju <ronkeashaju@gmail.com>
2023-01-03 11:28:38 +0000

    Python loops

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        ld = number % 10
+    else:
+        ld = number % -10
+        ld *= -1
+
+    print("{:d}".format(ld), end='')
+    return (ld)
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+
