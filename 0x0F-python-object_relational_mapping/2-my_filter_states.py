7b2c3a8a3f19a65a1cadfbbdf34f540b5720e067
Ronke Ashaju <ronkeashaju@gmail.com>
2023-04-22 20:01:37 +0000

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,16 +9,18 @@ if __name__ == '__main__':
             host="localhost",
             port=3306,
             user=argv[1],
-            passwd=argv[2],
+            password=argv[2],
             db=argv[3],
             charset="utf8"
         )
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE" + "CAST LIKE" +
+    db = conn.cursor()
+    db.execute("SELECT * FROM states WHERE" + "CAST LIKE" +
                 " CAST('{}'AS BINARY) ORDER BY id ASC;".format(argv[4]))
-    query_rows = cur.fetchall()
+    query_rows = db.fetchall()
 
     for row in query_rows:
         print(row)
 
-    cur.close(i)
+    db.close()
+    conn.close()
+
