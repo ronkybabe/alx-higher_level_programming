3e84732738fa2b864a906dcd1c263d02f4b9ea8c
Ronke Ashaju <ronkeashaju@gmail.com>
2023-04-28 14:36:27 +0000

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # A script that takes in url,sends request to that url and display the size
-curl -sI "$1" | awk ' /Content-Length/{print $2}'
+curl -sI "$1" | grep -oiE 'Content-Length: [0-9]+' | cut -d ' ' -f2
 
