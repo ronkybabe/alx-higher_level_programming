d9375738c5e24c85c331b1fe052ba6e6e21b8f89
Ronke Ashaju <ronkeashaju@gmail.com>
2023-04-29 14:01:56 +0000

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''
 Script that takes in a letter and sends a
-POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
+POST request to http://0.0.0.0:5000/search_user.
 '''
 import requests as req
 from sys import argv
@@ -9,7 +9,7 @@ from sys import argv
 
 if __name__ == '__main__':
     query = argv[1] if len(argv) > 1 else ""
-    r = req.post('http://0.0.0.0:5000/search_user', data={'q': query})
+    res = req.post('http://0.0.0.0:5000/search_user', data={'q': query})
     try:
         json_data = res.json()
         if json_data:
