c443340488729b18a83463cfc619b857f5d97b25
Ronke Ashaju <ronkeashaju@gmail.com>
2023-04-29 00:43:58 +0000

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Write a Python script that takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter.
+'''
+from urllib import request
+from urllib import parse
+from sys import argv
+
+
+if __name__ == '__main__':
+    if len(argv) > 2:
+        data = parse.urlencode({'email': argv[2]})
+        data = data.encode('ascii')
+        req = request.Request(argv[1], data)
+        with request.urlopen(req) as res:
+            print(res.read().decode('utf-8'))
