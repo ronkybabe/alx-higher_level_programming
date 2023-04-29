b3697090d9348d015fb92941334bce5422a2e1ba
Ronke Ashaju <ronkeashaju@gmail.com>
2023-04-29 13:29:09 +0000

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+Script that takes in a URL and an email address, sends a
+POST request to the passed URL with the email as a parameter.
+'''
+import requests as req
+from sys import argv
+
+
+if __name__ == '__main__':
+    if len(argv) > 2:
+        r = requests.post(argv[1], data={'email': argv[2]})
+        print(r.text)
